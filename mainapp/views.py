83dd8450a3bafeabba96b4d432f1deed9c540684
Jesus <jesusmagdaleno41@gmail.com>
2024-01-10 13:19:20 -0600

    Index terminado

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from certificates.models import *
+from experience.models import *
+from comments.models import *
 
 # Create your views here.
 def index(request):
 
     certificates = Certificate.objects.all()
-    data=[]
+    commentss = Comment.objects.all()
+    experience = Experience.objects.latest('end_date')
+    data_certificates=[]
+    data_comments = []
     for a in range(0,4,1):
-        data.append(certificates[a])
+        data_certificates.append(certificates[a])
+        data_comments.append(commentss[a])
     return render(request, 'mainapp/index.html',{
         'title':'Inicio',
-        'certificates':data
+        'certificates':data_certificates,
+        'comentarios':data_comments,
+        'experiencia':experience
     })
 
 
